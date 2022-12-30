eb91c7f6ba03555fc842e02662f1f6c7d3f8d3ae
Jiří Moravčík <jiri.moravcik@gmail.com>
2022-12-30 09:38:32 +0100

    revert MemoryStorage export

--- src/apify/__init__.py
+++ src/apify/__init__.py
@@ -1,5 +1,4 @@
 from ._version import __version__
 from .actor import Actor
-from .memory_storage.memory_storage import MemoryStorage
 
-__all__ = ['Actor', 'MemoryStorage', '__version__']
+__all__ = ['Actor', '__version__']
