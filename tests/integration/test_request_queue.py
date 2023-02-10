c10bbf4a3fdfb3bce7ad44189af3abd7709d63af
František Nesveda <frantisek@apify.com>
2023-02-10 11:31:59 +0100

    Add more logging to flaky integration test

--- tests/integration/test_request_queue.py
+++ tests/integration/test_request_queue.py
@@ -3,26 +3,32 @@ from apify import Actor
 from .conftest import ActorFactory
 
 
+# TODO: this test is flaky, and the actor often times out, figure out why and fix it
 class TestRequestQueue:
     async def test_simple(self, make_actor: ActorFactory) -> None:
         async def main() -> None:
             async with Actor:
                 desired_request_count = 100
+                print('Opening request queue...')
+                # I have seen it get stuck on this call
                 rq = await Actor.open_request_queue()
                 # Add some requests
                 for i in range(desired_request_count):
+                    print(f'Adding request {i}...')
                     await rq.add_request({
                         'url': f'https://example.com/{i}',
                     })
 
                 handled_request_count = 0
                 while next_request := await rq.fetch_next_request():
+                    print('Fetching next request...')
                     queue_operation_info = await rq.mark_request_as_handled(next_request)
                     assert queue_operation_info is not None
                     assert queue_operation_info['wasAlreadyHandled'] is False
                     handled_request_count += 1
 
                 assert handled_request_count == desired_request_count
+                print('Waiting for queue to be finished...')
                 is_finished = await rq.is_finished()
                 assert is_finished is True
 
