4cd9ceeee354ae771810eee0be7fafa6f783e6f8
Vlada Dusek <v.dusek96@gmail.com>
2024-03-12 15:58:21 +0100

    just fix the docstring...

--- src/apify/_utils.py
+++ src/apify/_utils.py
@@ -415,14 +415,14 @@ ListOrDictOrAny = TypeVar('ListOrDictOrAny', list, dict, Any)
 
 
 def compute_short_hash(data: bytes, *, length: int = 8) -> str:
-    """Computes a hexadecimal SHA-256 hash of the provided data and returns a substring of it.
+    """Computes a hexadecimal SHA-256 hash of the provided data and returns a substring (prefix) of it.
 
     Args:
         data: The binary data to be hashed.
         length: The length of the hash to be returned.
 
     Returns:
-        An 8-character hexadecimal hash.
+        A substring (prefix) of the hexadecimal hash of the data.
     """
     hash_object = sha256(data)
     return hash_object.hexdigest()[:length]
