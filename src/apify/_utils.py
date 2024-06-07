051373cae1b56e43aebaa05a6534bcea3062181e
Jan Buchar <Teyras@gmail.com>
2024-06-07 08:51:55 +0200

    Remove obsolete utils

--- src/apify/_utils.py
+++ src/apify/_utils.py
@@ -5,60 +5,20 @@ import builtins
 import contextlib
 import functools
 import inspect
-import json
-import mimetypes
 import os
-import re
 import sys
 import time
-from base64 import b64encode
 from collections import OrderedDict
 from collections.abc import MutableMapping
-from datetime import datetime, timezone
 from hashlib import sha256
 from importlib import metadata
 from logging import getLogger
-from typing import (
-    Any,
-    Callable,
-    Generic,
-    ItemsView,
-    Iterator,
-    NoReturn,
-    TypeVar,
-    ValuesView,
-    cast,
-    overload,
-)
+from typing import Any, Callable, Generic, ItemsView, Iterator, TypeVar, ValuesView, cast
 from typing import OrderedDict as OrderedDictType
 from urllib.parse import parse_qsl, urlencode, urlparse
 
-import aioshutil
 import psutil
-from aiofiles import ospath
-from aiofiles.os import remove, rename
-from apify_shared.consts import (
-    BOOL_ENV_VARS,
-    BOOL_ENV_VARS_TYPE,
-    DATETIME_ENV_VARS,
-    DATETIME_ENV_VARS_TYPE,
-    FLOAT_ENV_VARS,
-    FLOAT_ENV_VARS_TYPE,
-    INTEGER_ENV_VARS,
-    INTEGER_ENV_VARS_TYPE,
-    STRING_ENV_VARS_TYPE,
-    ActorEnvVars,
-    ApifyEnvVars,
-)
-from apify_shared.utils import (
-    ignore_docs,
-    is_content_type_json,
-    is_content_type_text,
-    is_content_type_xml,
-    maybe_extract_enum_member_value,
-)
-
-from apify.consts import REQUEST_ID_LENGTH, StorageTypes
+from apify_shared.utils import ignore_docs
 
 T = TypeVar('T')
 logger = getLogger(__name__)
@@ -115,75 +75,6 @@ class dualproperty(Generic[DualPropertyType]):  # noqa: N801
         return cast(DualPropertyType, val)
 
 
-@overload
-def fetch_and_parse_env_var(env_var: BOOL_ENV_VARS_TYPE) -> bool | None: ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: BOOL_ENV_VARS_TYPE, default: bool) -> bool:  # noqa: FBT001
-    ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: DATETIME_ENV_VARS_TYPE) -> datetime | str | None: ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: DATETIME_ENV_VARS_TYPE, default: datetime) -> datetime | str: ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: FLOAT_ENV_VARS_TYPE) -> float | None: ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: FLOAT_ENV_VARS_TYPE, default: float) -> float: ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: INTEGER_ENV_VARS_TYPE) -> int | None: ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: INTEGER_ENV_VARS_TYPE, default: int) -> int: ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: STRING_ENV_VARS_TYPE, default: str) -> str: ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: STRING_ENV_VARS_TYPE) -> str | None: ...
-
-
-@overload
-def fetch_and_parse_env_var(env_var: ActorEnvVars | ApifyEnvVars) -> Any: ...
-
-
-def fetch_and_parse_env_var(env_var: Any, default: Any = None) -> Any:
-    env_var_name = str(maybe_extract_enum_member_value(env_var))
-
-    val = os.getenv(env_var_name)
-    if not val:
-        return default
-
-    if env_var in BOOL_ENV_VARS:
-        return maybe_parse_bool(val)
-    if env_var in FLOAT_ENV_VARS:
-        parsed_float = maybe_parse_float(val)
-        if parsed_float is None:
-            return default
-        return parsed_float
-    if env_var in INTEGER_ENV_VARS:
-        parsed_int = maybe_parse_int(val)
-        if parsed_int is None:
-            return default
-        return parsed_int
-    if env_var in DATETIME_ENV_VARS:
-        return maybe_parse_datetime(val)
-    return val
-
-
 def get_cpu_usage_percent() -> float:
     return psutil.cpu_percent()
 
@@ -195,33 +86,6 @@ def get_memory_usage_bytes() -> int:
         with contextlib.suppress(psutil.NoSuchProcess):
             mem += int(child.memory_info().rss or 0)
     return mem
-
-
-def maybe_parse_bool(val: str | None) -> bool:
-    if val in {'true', 'True', '1'}:
-        return True
-    return False
-
-
-def maybe_parse_datetime(val: str) -> datetime | str:
-    try:
-        return datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc)
-    except ValueError:
-        return val
-
-
-def maybe_parse_float(val: str) -> float | None:
-    try:
-        return float(val)
-    except ValueError:
-        return None
-
-
-def maybe_parse_int(val: str) -> int | None:
-    try:
-        return int(val)
-    except ValueError:
-        return None
 
 
 async def run_func_at_interval_async(func: Callable, interval_secs: float) -> None:
@@ -239,64 +103,6 @@ async def run_func_at_interval_async(func: Callable, interval_secs: float) -> No
         res = func()
         if inspect.isawaitable(res):
             await res
-
-
-async def force_remove(filename: str) -> None:
-    """JS-like rm(filename, { force: true })."""
-    with contextlib.suppress(FileNotFoundError):
-        await remove(filename)
-
-
-def raise_on_non_existing_storage(client_type: StorageTypes, id: str) -> NoReturn:  # noqa: A002
-    client_type = maybe_extract_enum_member_value(client_type)
-    raise ValueError(f'{client_type} with id "{id}" does not exist.')
-
-
-def raise_on_duplicate_storage(client_type: StorageTypes, key_name: str, value: str) -> NoReturn:
-    client_type = maybe_extract_enum_member_value(client_type)
-    raise ValueError(f'{client_type} with {key_name} "{value}" already exists.')
-
-
-def guess_file_extension(content_type: str) -> str | None:
-    """Guess the file extension based on content type."""
-    # e.g. mimetypes.guess_extension('application/json ') does not work...
-    actual_content_type = content_type.split(';')[0].strip()
-
-    # mimetypes.guess_extension returns 'xsl' in this case, because 'application/xxx' is "structured"
-    # ('text/xml' would be "unstructured" and return 'xml')
-    # we have to explicitly override it here
-    if actual_content_type == 'application/xml':
-        return 'xml'
-
-    # Guess the extension from the mime type
-    ext = mimetypes.guess_extension(actual_content_type)
-
-    # Remove the leading dot if extension successfully parsed
-    return ext[1:] if ext is not None else ext
-
-
-def maybe_parse_body(body: bytes, content_type: str) -> Any:
-    if is_content_type_json(content_type):
-        return json.loads(body.decode('utf-8'))  # Returns any
-    if is_content_type_xml(content_type) or is_content_type_text(content_type):
-        return body.decode('utf-8')
-    return body
-
-
-def unique_key_to_request_id(unique_key: str) -> str:
-    """Generate request ID based on unique key in a deterministic way."""
-    request_id = re.sub(r'(\+|\/|=)', '', b64encode(sha256(unique_key.encode('utf-8')).digest()).decode('utf-8'))
-    return request_id[:REQUEST_ID_LENGTH] if len(request_id) > REQUEST_ID_LENGTH else request_id
-
-
-async def force_rename(src_dir: str, dst_dir: str) -> None:
-    """Rename a directory. Checks for existence of soruce directory and removes destination directory if it exists."""
-    # Make sure source directory exists
-    if await ospath.exists(src_dir):
-        # Remove destination directory if it exists
-        if await ospath.exists(dst_dir):
-            await aioshutil.rmtree(dst_dir, ignore_errors=True)
-        await rename(src_dir, dst_dir)
 
 
 ImplementationType = TypeVar('ImplementationType', bound=Callable)
@@ -361,45 +167,6 @@ class LRUCache(MutableMapping, Generic[T]):
 
 def is_running_in_ipython() -> bool:
     return getattr(builtins, '__IPYTHON__', False)
-
-
-@overload
-def budget_ow(value: str | float | bool, predicate: tuple[type, bool], value_name: str) -> None: ...
-
-
-@overload
-def budget_ow(value: dict, predicate: dict[str, tuple[type, bool]]) -> None: ...
-
-
-def budget_ow(
-    value: dict | str | float | bool,
-    predicate: dict[str, tuple[type, bool]] | tuple[type, bool],
-    value_name: str | None = None,
-) -> None:
-    """Budget version of ow."""
-
-    def validate_single(field_value: Any, expected_type: type, required: bool, name: str) -> None:  # noqa: FBT001
-        if field_value is None and required:
-            raise ValueError(f'"{name}" is required!')
-        if (field_value is not None or required) and not isinstance(field_value, expected_type):
-            raise ValueError(f'"{name}" must be of type "{expected_type.__name__}" but it is "{type(field_value).__name__}"!')
-
-    # Validate object
-    if isinstance(value, dict) and isinstance(predicate, dict):
-        for key, (field_type, required) in predicate.items():
-            field_value = value.get(key)
-            validate_single(field_value, field_type, required, key)
-    # Validate "primitive"
-    elif isinstance(value, (int, str, float, bool)) and isinstance(predicate, tuple) and value_name is not None:
-        field_type, required = predicate
-        validate_single(value, field_type, required, value_name)
-    else:
-        raise ValueError('Wrong input!')
-
-
-PARSE_DATE_FIELDS_MAX_DEPTH = 3
-PARSE_DATE_FIELDS_KEY_SUFFIX = 'At'
-ListOrDictOrAny = TypeVar('ListOrDictOrAny', list, dict, Any)
 
 
 def compute_short_hash(data: bytes, *, length: int = 8) -> str:
