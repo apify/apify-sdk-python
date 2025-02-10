5a60822b27e2bbe5311202abe4a830a69fdc1949
Jan Buchar <Teyras@gmail.com>
2025-02-10 11:17:50 +0100

    Update unit tests

--- tests/unit/actor/test_actor_log.py
+++ tests/unit/actor/test_actor_log.py
@@ -14,12 +14,8 @@ if TYPE_CHECKING:
     import pytest
 
 
-async def test_actor_logs_messages_correctly(
-    caplog: pytest.LogCaptureFixture,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
+async def test_actor_logs_messages_correctly(caplog: pytest.LogCaptureFixture) -> None:
     caplog.set_level(logging.DEBUG, logger='apify')
-    monkeypatch.setenv('APIFY_IS_AT_HOME', '1')
 
     with contextlib.suppress(RuntimeError):
         async with Actor(configure_logging=False):
@@ -43,7 +39,7 @@ async def test_actor_logs_messages_correctly(
             # Test that exception in Actor.main is logged with the traceback
             raise RuntimeError('Dummy RuntimeError')
 
-    assert len(caplog.records) == 12
+    assert len(caplog.records) == 13
 
     assert caplog.records[0].levelno == logging.INFO
     assert caplog.records[0].message == 'Initializing Actor...'
@@ -56,39 +52,42 @@ async def test_actor_logs_messages_correctly(
     assert getattr(caplog.records[1], 'os', None) == sys.platform
 
     assert caplog.records[2].levelno == logging.DEBUG
-    assert caplog.records[2].message.startswith('APIFY_ACTOR_EVENTS_WS_URL env var not set')
+    assert caplog.records[2].message == 'Event manager initialized'
 
     assert caplog.records[3].levelno == logging.DEBUG
-    assert caplog.records[3].message == 'Debug message'
+    assert caplog.records[3].message == 'Charging manager initialized'
 
-    assert caplog.records[4].levelno == logging.INFO
-    assert caplog.records[4].message == 'Info message'
+    assert caplog.records[4].levelno == logging.DEBUG
+    assert caplog.records[4].message == 'Debug message'
 
-    assert caplog.records[5].levelno == logging.WARNING
-    assert caplog.records[5].message == 'Warning message'
+    assert caplog.records[5].levelno == logging.INFO
+    assert caplog.records[5].message == 'Info message'
 
-    assert caplog.records[6].levelno == logging.ERROR
-    assert caplog.records[6].message == 'Error message'
+    assert caplog.records[6].levelno == logging.WARNING
+    assert caplog.records[6].message == 'Warning message'
 
     assert caplog.records[7].levelno == logging.ERROR
-    assert caplog.records[7].message == 'Exception message'
-    assert caplog.records[7].exc_info is not None
-    assert caplog.records[7].exc_info[0] is ValueError
-    assert isinstance(caplog.records[7].exc_info[1], ValueError)
-    assert str(caplog.records[7].exc_info[1]) == 'Dummy ValueError'
+    assert caplog.records[7].message == 'Error message'
 
-    assert caplog.records[8].levelno == logging.INFO
-    assert caplog.records[8].message == 'Multi\nline\nlog\nmessage'
+    assert caplog.records[8].levelno == logging.ERROR
+    assert caplog.records[8].message == 'Exception message'
+    assert caplog.records[8].exc_info is not None
+    assert caplog.records[8].exc_info[0] is ValueError
+    assert isinstance(caplog.records[8].exc_info[1], ValueError)
+    assert str(caplog.records[8].exc_info[1]) == 'Dummy ValueError'
 
-    assert caplog.records[9].levelno == logging.ERROR
-    assert caplog.records[9].message == 'Actor failed with an exception'
-    assert caplog.records[9].exc_info is not None
-    assert caplog.records[9].exc_info[0] is RuntimeError
-    assert isinstance(caplog.records[9].exc_info[1], RuntimeError)
-    assert str(caplog.records[9].exc_info[1]) == 'Dummy RuntimeError'
+    assert caplog.records[9].levelno == logging.INFO
+    assert caplog.records[9].message == 'Multi\nline\nlog\nmessage'
 
-    assert caplog.records[10].levelno == logging.INFO
-    assert caplog.records[10].message == 'Exiting Actor'
+    assert caplog.records[10].levelno == logging.ERROR
+    assert caplog.records[10].message == 'Actor failed with an exception'
+    assert caplog.records[10].exc_info is not None
+    assert caplog.records[10].exc_info[0] is RuntimeError
+    assert isinstance(caplog.records[10].exc_info[1], RuntimeError)
+    assert str(caplog.records[10].exc_info[1]) == 'Dummy RuntimeError'
 
-    assert caplog.records[11].levelno == logging.DEBUG
-    assert caplog.records[11].message == 'Not calling sys.exit(91) because Actor is running in an unit test'
+    assert caplog.records[11].levelno == logging.INFO
+    assert caplog.records[11].message == 'Exiting Actor'
+
+    assert caplog.records[12].levelno == logging.DEBUG
+    assert caplog.records[12].message == 'Not calling sys.exit(91) because Actor is running in an unit test'
