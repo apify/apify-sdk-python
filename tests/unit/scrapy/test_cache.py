381c0445067f97940e5c0b01fa83c3ab4216f946
Honza Javorek <mail@honzajavorek.cz>
2025-02-14 16:16:42 +0100

    feat(scrapy): add Scrapy cache using Apify KV store
    This code has been originally developed for the
    https://github.com/juniorguru/plucker/ project, which is licensed under
    AGPL-3.0-only. I am the sole author of that code and hereby I grant
    the https://github.com/apify/apify-sdk-python/ project the right to use
    it under an Apache-2.0 license, without the copyleft taking effect.
    My intention is to contribute this code to the upstream, remove
    it from my project, and only import the component from the apify
    package as a dependency, as I believe this component could be useful
    to other users of the apify package.

---
+++ tests/unit/scrapy/test_cache.py
@@ -0,0 +1,35 @@
+from time import time
+
+from apify.scrapy.cache import from_gzip, read_gzip_time, to_gzip
+
+FIXTURE_BYTES = (
+    b"\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\xffk`\x99*\xcc\x00\x01\xb5SzX\xf2\x12s"
+    b"S\xa7\xf4\xb0:\xe6d&\xa7N)\xd6\x03\x00\x1c\xe8U\x9c\x1e\x00\x00\x00"
+)
+
+
+def test_gzip() -> None:
+    assert from_gzip(to_gzip({"name": "Alice"})) == {"name": "Alice"}
+
+
+def test_to_gzip() -> None:
+    data_bytes = to_gzip({"name": "Alice"}, mtime=0)
+
+    assert data_bytes == FIXTURE_BYTES
+
+
+def test_from_gzip() -> None:
+    data_dict = from_gzip(FIXTURE_BYTES)
+
+    assert data_dict == {"name": "Alice"}
+
+
+def test_read_gzip_time() -> None:
+    assert read_gzip_time(FIXTURE_BYTES) == 0
+
+
+def test_read_gzip_time_non_zero() -> None:
+    current_time = int(time())
+    data_bytes = to_gzip({"name": "Alice"}, mtime=current_time)
+
+    assert read_gzip_time(data_bytes) == current_time
