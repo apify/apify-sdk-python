b83ad668be8fbdd6918510777e90b88409ef5450
Josef Prochazka <josef.prochazka@apify.com>
2025-03-24 14:56:14 +0100

    Update scrapy test

--- tests/integration/test_actor_scrapy.py
+++ tests/integration/test_actor_scrapy.py
@@ -41,7 +41,7 @@ async def test_actor_scrapy_title_spider(
 
     items = await actor.last_run().dataset().list_items()
 
-    assert items.count >= 10
+    assert items.count >= 9
 
     for item in items.items:
         assert 'url' in item
