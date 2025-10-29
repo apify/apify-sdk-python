90d95c9a5940186a144e6ad52e9f8925c3dc5faf
Max Bohomolov <moriturus7@gmail.com>
2025-10-29 17:37:46 +0000

    up comment

--- src/apify/_models.py
+++ src/apify/_models.py
@@ -107,7 +107,7 @@ class ActorRun(BaseModel):
     is_status_message_terminal: Annotated[bool | None, Field(alias='isStatusMessageTerminal')] = None
     meta: Annotated[ActorRunMeta, Field(alias='meta')]
     # Optionality, does not correspond to the documentation about 'stats' - https://docs.apify.com/api/v2/actor-run-get
-    # But at the moment, `stats` may be missing from the API response.
+    # But at the moment, `stats` may be missing in the API response.
     stats: Annotated[ActorRunStats | None, Field(alias='stats')] = None
     options: Annotated[ActorRunOptions, Field(alias='options')]
     build_id: Annotated[str, Field(alias='buildId')]
