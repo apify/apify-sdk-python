41ece4b5694766f991933ce410b89b679b222472
Vlada Dusek <v.dusek96@gmail.com>
2026-02-19 18:11:06 +0100

    Address feedback

--- src/apify/scrapy/_actor_runner.py
+++ src/apify/scrapy/_actor_runner.py
@@ -15,8 +15,23 @@ def run_scrapy_actor(coro: Coroutine) -> None:
     event loops, enabling the Apify and Scrapy integration to work together.
     """
     from scrapy.utils.reactor import install_reactor  # noqa: PLC0415
+    from twisted.internet.error import ReactorAlreadyInstalledError  # noqa: PLC0415
 
-    install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
+    try:
+        install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
+    except ReactorAlreadyInstalledError:
+        from twisted.internet import reactor as installed_reactor  # noqa: PLC0415
+
+        reactor_cls = type(installed_reactor)
+        if (
+            reactor_cls.__module__ != 'twisted.internet.asyncioreactor'
+            or reactor_cls.__name__ != 'AsyncioSelectorReactor'
+        ):
+            raise RuntimeError(
+                'A Twisted reactor is already installed and it is not AsyncioSelectorReactor. '
+                'Make sure that run_scrapy_actor() is called before importing any Scrapy or Twisted '
+                'modules that install a reactor (e.g. scrapy.crawler).'
+            ) from None
 
     from twisted.internet.defer import Deferred  # noqa: PLC0415
     from twisted.internet.task import react  # noqa: PLC0415
